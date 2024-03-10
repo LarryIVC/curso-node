90857556611a7712972d2a5cd58e22b9ceab6d22
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-03-09 20:10:24 -0400

    Add path module usage

---
+++ 5.path.js
@@ -0,0 +1,16 @@
+const path = require('node:path');
+// barra separadora de directorios
+console.log(path.sep)
+
+// unir rutas
+const filePath = path.join('content', 'subfolder', 'test.txt')
+console.log(filePath)
+
+const base = path.basename('content/subfolder/test.txt')
+console.log(base)
+
+const filename = path.basename('content/subfolder/test.txt', '.txt')
+console.log(filename)
+
+const extension = path.extname('content.subfolder.test.txt')
+console.log(extension)
