4be724c31f0b825157bf882821fc4677426bfb09
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-03-10 20:03:23 -0400

    Delete unnecessary files and modules

--- cjs/index.js
+++
@@ -1,5 +0,0 @@
-const sum = require('./sum.js')
-
-// console.log(globalThis)
-
-console.log(sum(1, 2))
--- mjs/index.mjs
+++
@@ -1,7 +0,0 @@
-// .js -> por defecto usa CommonJS
-// .mjs -> por defecto usa ES Modules
-// .cjs -> forzar CommonJS
-
-import { sum } from './sum.mjs'
-
-console.log(sum(1, 2))
