4183a2a6ef8147887ed6610e271e6ffb8c7827e2
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-03-20 19:58:57 -0400

    Add movie validation functions

---
+++ clase-5/validate.js
@@ -0,0 +1,41 @@
+import z from 'zod'
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie title must be a string',
+    required_error: 'Movie title is required'
+  }),
+  year: z.number().min(1900).max(2030).int(),
+  director: z.string(),
+  duration: z.number(),
+  genre: z.array(z.enum([
+    'Action',
+    'Adventure',
+    'Animation',
+    'Biography',
+    'Comedy',
+    'Crime',
+    'Documentary',
+    'Drama',
+    'Family',
+    'Fantasy',
+    'Sci-Fi'
+  ]),
+  {
+    required_error: 'Movie genre is required'
+  }
+  ),
+  rate: z.number().min(0).max(10).default(5),
+  poster: z.string().url({
+    message: 'Poster must be a valid URL'
+  })
+
+})
+
+export const validateMovie = (object) => {
+  return movieSchema.safeParse(object)
+}
+
+export const validatePartialMovie = (object) => {
+  return movieSchema.partial().safeParse(object)
+}
