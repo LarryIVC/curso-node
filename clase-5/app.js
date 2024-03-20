149cb8a1215a5855106cb24237a543188d483534
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-03-20 19:58:23 -0400

    Add movie routes and middleware to app.js

---
+++ clase-5/app.js
@@ -0,0 +1,20 @@
+import express, { json } from 'express'
+import { movieRouter } from './routes/movies.js'
+import { corsMiddleware } from './middlewares/cors.js'
+
+const PORT = process.env.PORT || 1234
+
+const app = express()
+app.disable('x-powered-by')
+app.use(json())
+app.use(corsMiddleware())
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hola mundo</h1>')
+})
+
+app.use('/api/v1/movies', movieRouter)
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en  http://localhost:${PORT}`)
+})
