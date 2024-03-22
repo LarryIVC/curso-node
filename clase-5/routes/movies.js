7f555ee57f2dec3957cfe339fba1e589e8a37903
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-03-22 00:24:42 -0400

    Refactor movieRouter in movies.js

--- clase-5/routes/movies.js
+++ clase-5/routes/movies.js
@@ -1,14 +1,20 @@
 import { Router } from 'express'
 import { MovieController } from '../controllers/movies.js'
+// import { MovieModel } from '../models/mysql/movie.js'
 
-export const movieRouter = Router()
+export const CretaeMovieRouter = ({ MovieModel }) => {
+  const movieRouter = Router()
+  const movieController = new MovieController({ MovieModel })
 
-movieRouter.get('/', MovieController.getAll)
+  movieRouter.get('/', movieController.getAll)
 
-movieRouter.get('/:id', MovieController.getById)
+  movieRouter.get('/:id', movieController.getById)
 
-movieRouter.post('/', MovieController.create)
+  movieRouter.post('/', movieController.create)
 
-movieRouter.patch('/:id', MovieController.update)
+  movieRouter.patch('/:id', movieController.update)
 
-movieRouter.delete('/:id', MovieController.delete)
+  movieRouter.delete('/:id', movieController.delete)
+
+  return movieRouter
+}
