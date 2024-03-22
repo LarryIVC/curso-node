e869718d19910925baf093667dd64e309e1287f2
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-03-22 00:24:29 -0400

    Refactor movie controller to use dependency injection

--- clase-5/controllers/movies.js
+++ clase-5/controllers/movies.js
@@ -1,41 +1,47 @@
-import { MovieModel } from '../models/mysql/movie.js'
 import { validateMovie, validatePartialMovie } from '../validate.js'
 
 export class MovieController {
-  static async getAll (req, res) {
+  constructor ({ MovieModel }) {
+    this.MovieModel = MovieModel
+  }
+
+  getAll = async (req, res) => {
     const { genre } = req.query
-    const movies = await MovieModel.getAll({ genre })
+    const movies = await this.MovieModel.getAll({ genre })
     res.json(movies)
   }
 
-  static async getById (req, res) {
+  getById = async (req, res) => {
     const { id } = req.params
-    const movie = await MovieModel.getById({ id })
+    const movie = await this.MovieModel.getById({ id })
     if (!movie) {
       return res.status(404).json({ error: 'Movie not found' })
     }
     res.json(movie)
   }
 
-  static async create (req, res) {
+  create = async (req, res) => {
     const result = validateMovie(req.body)
     // console.log(result)
     if (result.error) {
       return res.status(400).json({ error: JSON.parse(result.error.message) })
     }
 
-    const newMovie = await MovieModel.create({ input: result.data })
+    const newMovie = await this.MovieModel.create({ input: result.data })
     if (!newMovie) {
       return res.status(400).json({ error: 'Movie not created' })
     }
     res.status(201).json(newMovie)
   }
 
-  static async update (req, res) {
+  update = async (req, res) => {
     const { id } = req.params
     const result = validatePartialMovie(req.body)
 
-    const updatedMovie = await MovieModel.update({ id, input: result.data })
+    const updatedMovie = await this.MovieModel.update({
+      id,
+      input: result.data
+    })
 
     if (!updatedMovie) {
       return res.status(400).json({ error: 'Movie not found' })
@@ -43,10 +49,10 @@ export class MovieController {
     res.json(updatedMovie)
   }
 
-  static async delete (req, res) {
+  delete = async (req, res) => {
     const { id } = req.params
 
-    const response = await MovieModel.delete({ id })
+    const response = await this.MovieModel.delete({ id })
 
     if (!response) {
       return res.status(404).json({ error: 'Movie not found' })
